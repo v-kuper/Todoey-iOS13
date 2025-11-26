40432ade73549e293ced4bdfa71c8279e0473507
v-kuper <vkuper7@gmail.com>
2025-11-27 00:34:59 +0300

    init

---
+++ Todoey/ToDoListViewControler.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewControler: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgo"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+}
